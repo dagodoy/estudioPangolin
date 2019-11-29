3bf7d41c530437a15693d1788f850743c1b64353
David Godoy Ruiz <dagodoy@ucm.es>
2019-11-29 18:16:40 +0100

    Sistema de combo completo

--- docs/player.js
+++ docs/player.js
@@ -12,17 +12,24 @@ export default class Player extends Character{
         this.lifeDelay = 1000;
         this.lifeCD = 0;
 
-        this.attackDelay = 1000;
-        this.comboDelay = -900;
-        this.comboCount = 0;
-        this.attackCD = 0;
         this.attackControl = false;
+
+        this.attackDelay = 300;
+        this.attackDelayBase = 300;
+        this.attackCD = 0;
+
+        this.comboDelay = 1000;
+        this.comboCount = 0; 
+        this.comboCD = 0;
+        this.comboResetter = 500;
+        this.comboDuration = 3;
+        
+
+        this.startingDmg = atkDmg;
+        this.atkFinisher = 2*atkDmg;
 
         this.body.label = 'player';
         this.hitbox.label = 'playerHitbox';
-
-        
-
     }
     
     playerController(){
@@ -56,6 +63,7 @@ export default class Player extends Character{
       if (this.cursors.space.isDown && this.attackControl){     
         this.hitbox.active = true;
         this.attackControl = false;
+        console.log("ataque" + this.comboCount);
       }
       else{
         this.hitbox.active = false;
@@ -68,6 +76,45 @@ export default class Player extends Character{
         this.speed += spd;
         this.speedy = true;
     }
+    loseLife(t){
+      if (t - this.lifeCD > this.lifeDelay){    //Pierde vida periódicamente
+        this.reduceEnergy();
+        this.lifeCD = t;
+      }
+    }
+    makeSpeedy(){
+      if(!this.inBattle && !this.speedy){   //para cambiar el booleano hay que hacer primero el sistema de zonas, no sé como lo cambiaremos aún
+        this.changeSpeed(this.speed);
+      }
+    }
+
+    attackSystem(t){
+      if(!this.attackControl){
+        if (t - this.attackCD > this.attackDelay){    //Controla el cooldown del ataque
+          if (this.comboCount === this.comboDuration){
+            this.attackDelay = attackDelayBase;
+            this.comboCount = 0;
+          } 
+          this.comboCount++;
+          this.attackControl = true;
+  
+          if (this.comboCount > this.comboDuration - 1){
+            this.attackDelay = this.comboDelay;
+          } 
+          this.attackCD = t;
+        }
+        this.comboCD = t;
+      }
+      else{
+        if (this.comboCount < this.ComboDuration && t-this.comboCD > this.comboResetter){
+          this.comboCount = 1;
+          this.comboCD = t;
+        }   
+        this.attackCD = t;
+      }
+      if (comboCount === this.ComboDuration - 1) this.atkDmg = this.atkFinisher;
+      else this.atkDmg = this.startingDmg;
+    }
 
     reduceEnergy(){
       this.reduceHealth(5);   //estaría mejor decir por constructora mediante una variable la cantidad constante a reducir
@@ -77,23 +124,8 @@ export default class Player extends Character{
         this.playerController();
         console.log(this.hitbox.active);  
         this.moveHitbox();
-        if(!this.inBattle && !this.speedy){   //para cambiar el booleano hay que hacer primero el sistema de zonas, no sé como lo cambiaremos aún
-          this.changeSpeed(this.speed);
-        }
-        if (t - this.lifeCD > this.lifeDelay){    //Pierde vida periódicamente
-          this.reduceEnergy();
-          this.lifeCD = t;
-        }
-        if (t - this.attackCD + this.comboDelay > this.attackDelay){    //Controla el cooldown del ataque
-          this.comboCount++;
-          this.attackControl = true;
-          if (this.comboCount > 2){
-            this.comboDelay = -900;
-          } 
-          else{
-            this.comboDelay = 0;
-          }
-          this.attackCD = t;
-        }
+        this.makeSpeedy();
+        this.loseLife(t);
+        this.attackSystem(t);
     }
 }
