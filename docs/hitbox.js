05b02ebb753b1231cb482c74ca6fda2c18b1e04a
David Godoy <48752117+dagodoy@users.noreply.github.com>
2019-12-13 19:29:00 +0100

    Las hitboxes rotan en torno al personaje

--- docs/hitbox.js
+++ docs/hitbox.js
@@ -1,5 +1,5 @@
 export default class Hitbox extends Phaser.Physics.Matter.Sprite {
-    constructor(scene, x, y, offset, spriteSheet, character, area) {
+    constructor(scene, x, y, offset, spriteSheet, character, area, isAttack) {
         super(scene.matter.world, x, y, spriteSheet);
         //this.body.destroy();
         console.log(this.body)
@@ -7,24 +7,18 @@ export default class Hitbox extends Phaser.Physics.Matter.Sprite {
         this.body.gameObject = this;
         this.character = character;
         this.offset = offset;
+        this.isAttack = isAttack;
         
         this.scene.add.existing(this);
         this.scene.matter.world.add(this);
 
         this.active = false;
         this.body.isSensor = true;
-
-        this.bool = true;
-  
     }
     moveHitbox(dirx, diry){
         let mod = Math.sqrt(dirx*dirx + diry * diry); 
         this.x = this.character.body.position.x + this.offset*dirx/mod;
         this.y = this.character.body.position.y + this.offset*diry/mod;
-        if (this.bool){
-            this.scene.matter.body.rotate(this.body, Math.PI/2)
-            this.bool = false;
-        }
-
+        if (this.isAttack) this.rotation = Math.atan(diry/dirx)
     }
 }
