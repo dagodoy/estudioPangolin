e4a08140b61a927f691637b10d381a2fe483a4f1
jorgbell <48956755+jorgbell@users.noreply.github.com>
2019-12-18 01:01:08 +0100

    BARRA DE VIDA PROVISIONAL YAAAY

--- docs/lifebar.js
+++ docs/lifebar.js
@@ -3,18 +3,20 @@ export default class Lifebar extends Phaser.Physics.Matter.Sprite {
     super(scene.matter.world, x, y, 'lifebar');
     this.scene.add.existing(this);
 
+    this.setScrollFactor(0,0);  //no se mueve, se mantiene fija (UI)
     this.maxHealth = maxHealth;
   }
 
 
-  reduceBar(damage, health){
+  reduceBar(damage){
     //sacar el porcentaje de daño con respecto a la vida total
     this.relation = damage / this.maxHealth;
     console.log(this.relation);
     //restar ese porcentaje a la imagen
     this.percent = this.width*this.relation;
-    console.log(this.percent);
-    this.setDisplaySize(this.width-(this.percent), this.height);
+    this.width = this.width-this.percent;
+    this.setDisplaySize(5*(this.width), 5*(this.height));
+    this.setPosition(this.x-5*(this.percent/2), this.y)
     
   }
 }
