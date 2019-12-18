6011d9ffcdc0f4e6c552c3130a8e2a4f0231defe
jorgbell <48956755+jorgbell@users.noreply.github.com>
2019-12-18 04:37:52 +0100

    Añadidas cosas del boton

--- docs/scripts/game_over.js
+++ docs/scripts/game_over.js
@@ -3,11 +3,13 @@
 export default class GameOver extends Phaser.Scene {
   constructor() {
     super({ key: 'game_over' });
+    $(this).on("pointerdown", this.changeScene(), this);
   }
   
   preload() {  
-    this.screen = this.load.image('screen', 'images/game_over.png');
+    this.load.image('screen', 'images/game_over.png');
     this.load.atlas('vampire', 'images/vampireatlas.png', 'json/vampireatlas_atlas.json');
+    this.load.image('button', 'images/button.jpg');
   }
 
   create() {
@@ -19,10 +21,16 @@ export default class GameOver extends Phaser.Scene {
       frames: this.anims.generateFrameNames('vampire',  {prefix: 'vampire64_', start: 72, end:80}),
       frameRate: 6})
       this.vampire.play('die');
+    this.button = this.matter.add.sprite(700,300,'button');
+    this.button.setScale(0.5,0.5);
+    this.input.mouse.capture = true;
       
   }
 
-
+changeScene(){
+  console.log('e');
+  this.start('main');
+}
   update(time, delta) {
   }
 
